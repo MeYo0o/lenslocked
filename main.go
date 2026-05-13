26bfd8805ead2003806fdadbe26576d3938dd4cb
Moaz Ahmed <justmeyo2025@gmail.com>
2026-05-13 14:17:07 +0300

    add: implement home and contact handlers with HTML response

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 	"net/http"
 )
 
-// * a single route to be registered
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+// * routes to be registered
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, `<h1>Contact Page</h1>
+	<p>To get in touch, email me at <a href="mailto:moaz@innolabs.ai">moaz@innolabs.ai</a></p>`)
 }
 
 func main() {
@@ -17,7 +24,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	//* register routes
-	mux.HandleFunc("/", handlerFunc)
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/contact", contactHandler)
 
 	fmt.Println("starting server at :3000")
 

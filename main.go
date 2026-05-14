29ec2bde9d47b723665fd516f0fecc199c26062d
Moaz Ahmed <justmeyo2025@gmail.com>
2026-05-15 02:11:12 +0300

    add: implement FAQ handler and update routing logic

--- main.go
+++ main.go
@@ -19,27 +19,46 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	<p>To get in touch, email me at <a href="mailto:moaz@innolabs.ai">moaz@innolabs.ai</a></p>`)
 }
 
-func readPathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.RawPath {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.NotFound(w, r)
-	}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, `
+<h1>FAQ Page</h1>
+<h3>Q: Is there a free version?</h3>
+<p>A: Yes! We offer a free trial for 30 days on any paid plans.</p>
+
+<h3>Q: What are your support hours?</h3>
+<p>A: We have support staff answering emails 24/7, though response times may be a
+bit slower on weekends.</p>
+
+<h3>Q: How do I contact support?</h3>
+<p>A: Email us - <a href="mailto:support@innolabs.ai">support@innolabs.ai</a></p>
+	`)
 }
+
+// func readPathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.RawPath {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.NotFound(w, r)
+// 	}
+// }
 
 // * Making my own router
 type MeYoRouter struct{}
 
 // * essential method to be created to implement the Handler Interface is the ServeHTTP method.
 func (mr MeYoRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.RawPath {
+	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.NotFound(w, r)
 	}
@@ -47,7 +66,7 @@ func (mr MeYoRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	//* new instance of my new created router
-	mr := MeYoRouter{}
+	var mr MeYoRouter
 
 	//* Define a new server
 	// mux := http.NewServeMux()
@@ -60,7 +79,7 @@ func main() {
 	fmt.Println("starting server at :3000")
 
 	//* starting the server
-	err := http.ListenAndServe(":3000", mr)
+	err := http.ListenAndServe(":3000", http.HandlerFunc(mr.ServeHTTP))
 	if err != nil {
 		log.Fatalf("couldn't start the server.")
 	}

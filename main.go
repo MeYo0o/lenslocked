202d0a44213385ef5182e323b0f8d21f99f92988
Moaz Ahmed <justmeyo2025@gmail.com>
2026-05-15 01:27:03 +0300

    add: my new own custom router.

--- main.go
+++ main.go
@@ -20,7 +20,22 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func readPathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
+	switch r.URL.RawPath {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+// * Making my own router
+type MeYoRouter struct{}
+
+// * essential method to be created to implement the Handler Interface is the ServeHTTP method.
+func (mr MeYoRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.RawPath {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
@@ -31,18 +46,21 @@ func readPathHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//* new instance of my new created router
+	mr := MeYoRouter{}
+
 	//* Define a new server
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
 
 	//* register routes
-	mux.HandleFunc("/", readPathHandler)
+	// mux.HandleFunc("/", readPathHandler)
 	// mux.HandleFunc("/", homeHandler)
 	// mux.HandleFunc("/contact", contactHandler)
 
 	fmt.Println("starting server at :3000")
 
 	//* starting the server
-	err := http.ListenAndServe(":3000", mux)
+	err := http.ListenAndServe(":3000", mr)
 	if err != nil {
 		log.Fatalf("couldn't start the server.")
 	}

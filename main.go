dcb3d0f1a1c3b76131f75208abc82e0371e9f61c
Moaz Ahmed <justmeyo2025@gmail.com>
2026-05-16 18:49:02 +0300

    refactor: replace custom router implementation with chi router and update route handling

--- main.go
+++ main.go
@@ -4,7 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
+
+func main() {
+	//* New chi Router
+	r := chi.NewRouter()
+
+	//* Setup Routes for chi
+	setupRoutes(r)
+
+	fmt.Println("starting server at :3000")
+
+	//* starting the server
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		log.Fatalf("couldn't start the server.")
+	}
+}
+
+func setupRoutes(chi *chi.Mux) {
+	chi.Get("/", homeHandler)
+	chi.Get("/contact", contactHandler)
+	chi.Get("/faq", faqHandler)
+}
 
 // * routes to be registered
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -34,53 +58,4 @@ bit slower on weekends.</p>
 <h3>Q: How do I contact support?</h3>
 <p>A: Email us - <a href="mailto:support@innolabs.ai">support@innolabs.ai</a></p>
 	`)
-}
-
-// func readPathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.RawPath {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.NotFound(w, r)
-// 	}
-// }
-
-// * Making my own router
-type MeYoRouter struct{}
-
-// * essential method to be created to implement the Handler Interface is the ServeHTTP method.
-func (mr MeYoRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.NotFound(w, r)
-	}
-}
-
-func main() {
-	//* new instance of my new created router
-	var mr MeYoRouter
-
-	//* Define a new server
-	// mux := http.NewServeMux()
-
-	//* register routes
-	// mux.HandleFunc("/", readPathHandler)
-	// mux.HandleFunc("/", homeHandler)
-	// mux.HandleFunc("/contact", contactHandler)
-
-	fmt.Println("starting server at :3000")
-
-	//* starting the server
-	err := http.ListenAndServe(":3000", http.HandlerFunc(mr.ServeHTTP))
-	if err != nil {
-		log.Fatalf("couldn't start the server.")
-	}
 }

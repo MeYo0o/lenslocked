dbe748318f16a15df0897fd3a85156269aff7356
Moaz Ahmed <justmeyo2025@gmail.com>
2026-05-28 08:58:53 +0300

    fix: update email address in contact template

--- templates/contact_templ.go
+++ templates/contact_templ.go
@@ -29,7 +29,7 @@ func Contact() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:moaz@innolabs.ai\">moaz@innolabs.ai</a></p>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:support@innolabs.ai\">moaz@innolabs.ai</a></p>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}

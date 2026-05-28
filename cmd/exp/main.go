e746d1cb0e56c103fe0394bdbc29a7fd81664baa
Moaz Ahmed <justmeyo2025@gmail.com>
2026-05-28 03:46:43 +0300

    fix: update user struct to include Bio field and improve error logging in template parsing

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -9,6 +9,7 @@ import (
 type User struct {
 	Name string
 	Age  int
+	Bio  string
 	Meta UserMeta
 }
 
@@ -17,14 +18,15 @@ type UserMeta struct {
 }
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	t, err := template.ParseFiles("./cmd/exp/hello.gohtml")
 	if err != nil {
-		log.Panicln("couldn't parse the template file")
+		log.Panicln("couldn't parse the template file:", err)
 	}
 
 	user := User{
 		Name: "Moaz",
 		Age:  33,
+		Bio:  "Software Engineer",
 		Meta: UserMeta{
 			Visits: 3,
 		},
@@ -32,6 +34,6 @@ func main() {
 
 	err = t.Execute(os.Stdout, user)
 	if err != nil {
-		log.Panicln("couldn't Execute the template file")
+		log.Panicln("couldn't Execute the template file:", err)
 	}
 }
